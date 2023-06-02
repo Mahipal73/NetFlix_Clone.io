60cc35f893e3d1104a24ebd1f57e9c0df56bfa01
Mahipal73 <116993931+Mahipal73@users.noreply.github.com>
2023-06-03 00:25:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 /* 
 🌟 APP: Make Netflix
 
-Here we have the Netflix app but it's up to you to make it work by pulling all the movies using an API!
-
 Create a fetchMovies() function that will make a dynamic API call to what you need 👇
 ========================================
 
@@ -12,20 +10,17 @@ Create a fetchMovies() function that will make a dynamic API call to what you ne
 
 
 
-These are the 3 main functions and their URL'S you must create  👇
+These are the 3 main functions you must create 👇
 ========================================
 
 - getOriginals()
-  * URL : 'https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213'
 
 - getTrendingNow()
-  * URL : 'https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045'
 
 - getTopRated()
-  * URL : 'https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1'
 
 
-** These functions will provide the URL you need to fetch() movies of that genere **
+** These functions will provide the URL you need to fetch movies of that genere **
 
 These are all the DIV ID's you're gonna need access to 👇
 ========================================================
@@ -42,78 +37,118 @@ window.onload = () => {
   }
   
   // ** Helper function that makes dynamic API calls **
+  // path_type 👉 (backdrop, poster)
+  // dom_element 👉 (trending, top rated)
+  // fetchMovies('https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1', 'top_rated', 'backdrop_path')
   function fetchMovies(url, dom_element, path_type) {
-    // Use Fetch with the url passed down 
-  
-    // Within Fetch get the response and call showMovies() with the data , dom_element, and path type
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error('something went wrong')
+        }
+      })
+      .then(data => {
+        showMovies(data, dom_element, path_type)
+      })
+      .catch(error_data => {
+        console.log(error_data)
+      })
   }
   
   //  ** Function that displays the movies to the DOM **
   showMovies = (movies, dom_element, path_type) => {
     
     // Create a variable that grabs id or class
-  
+    var moviesEl = document.querySelector(dom_element)
   
     // Loop through object
-  
+    for (var movie of movies.results) {
   
       // Within loop create an img element
-  
+      var imageElement = document.createElement('img')
   
       // Set attribute
-  
+      imageElement.setAttribute('data-id', movie.id)
   
       // Set source
-  
+      imageElement.src = `https://image.tmdb.org/t/p/original${movie[path_type]}`
   
       // Add event listener to handleMovieSelection() onClick
-  
-    
+      imageElement.addEventListener('click', e => {
+        handleMovieSelection(e)
+      })
       // Append the imageElement to the dom_element selected
-  
+      moviesEl.appendChild(imageElement)
     }
   }
   
   // ** Function that fetches Netflix Originals **
   function getOriginals() {
-  
+    var url =
+      'https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213'
+    fetchMovies(url, '.original__movies', 'poster_path')
   }
   // ** Function that fetches Trending Movies **
   function getTrendingNow() {
-  
+    var url =
+      'https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045'
+    fetchMovies(url, '#trending', 'backdrop_path')
   }
   // ** Function that fetches Top Rated Movies **
   function getTopRated() {
-  
+    var url =
+      'https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1'
+    fetchMovies(url, '#top_rated', 'backdrop_path')
   }
   
   // ** BONUS **
   
-  // ** Fetches URL provided and returns response.json()
   async function getMovieTrailer(id) {
-    //URL: `https://api.themoviedb.org/3/movie/${id}/videos?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US`
-  
+    var url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US`
+    return await fetch(url).then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error('something went wrong')
+      }
+    })
   }
   
-  // ** Function that adds movie data to the DOM
   const setTrailer = trailers => {
-    // Set up iframe variable to hold id of the movieTrailer Element
-    const iframe
-    // Set up variable to select .movieNotFound element
-    const movieNotFound
-  
-    // If there is a trailer add the src for it
+    const iframe = document.getElementById('movieTrailer')
+    const movieNotFound = document.querySelector('.movieNotFound')
     if (trailers.length > 0) {
-      // add d-none class to movieNotFound and remove it from iframe
-  
-      // add youtube link with trailers key to iframe.src
+      movieNotFound.classList.add('d-none')
+      iframe.classList.remove('d-none')
+      iframe.src = `https://www.youtube.com/embed/${trailers[0].key}`
     } else {
-      // Else remove d-none class to movieNotfound and ADD it to iframe
-  
+      iframe.classList.add('d-none')
+      movieNotFound.classList.remove('d-none')
     }
   }
   
+  const handleMovieSelection = e => {
+    const id = e.target.getAttribute('data-id')
+    const iframe = document.getElementById('movieTrailer')
+    // here we need the id of the movie
+    getMovieTrailer(id).then(data => {
+      const results = data.results
+      const youtubeTrailers = results.filter(result => {
+        if (result.site == 'YouTube' && result.type == 'Trailer') {
+          return true
+        } else {
+          return false
+        }
+      })
+      setTrailer(youtubeTrailers)
+    })
   
+    // open modal
+    $('#trailerModal').modal('show')
+    // we need to call the api with the ID
+  }
   
   
   
